f6d46210c325ca355d5cfd59b471c5751d4e4d28
jkwatson92 <jourdanwatson@gmail.com>
2021-12-07 18:39:10 -0500

    Day 2 completed

--- Day2/day2.py
+++ Day2/day2.py
@@ -6,22 +6,24 @@ my_file.close()
 
 x_pos=0
 y_pos=0
-##Forward = +x, down = +y, up= -y
+aim=0
+##Forward = +x, down = +y +aim, up= -y -aim
 
 for i in range(0,len(content_list)-1):
-    
     movement=[int(j) for j in content_list[i].split() if j.isdigit()]
    
     if "forward" in content_list[i]:
         x_pos+=movement[0]
+        y_pos+=(aim*movement[0])
 
     elif "up" in content_list[i]:
-        y_pos-=movement[0]
+        aim-=movement[0]
 
     elif "down" in content_list[i]:
-        y_pos+=movement[0]
+        aim+=movement[0]
+    
 
-print("x: ", x_pos, " y: ", y_pos)
+print("x: ", x_pos, " y: ", y_pos, " aim: ", aim)
 print("multiplied: ", x_pos*y_pos)        
         
         
